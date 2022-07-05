a437aabed2418a774ab908c2e5ff87805b57a17d
Aravindan <108742783+AravindDevop@users.noreply.github.com>
2022-07-05 19:42:42 +0530

    Add files via upload

---
+++ Bitwiseoperater.cpp
@@ -0,0 +1,22 @@
+// C++ program to demonstrate working of unary increment
+// and decrement operators
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int a = 36;
+int b = 35;
+cout<<"BITWISE OPERATOR\n";
+cout<<"~a is "<<~a<<endl;
+cout<<"a&b is "<<(a&b)<<endl;
+cout<<"a|b is "<<(a|b)<<endl;
+cout<<"a^b is "<<(a^b)<<endl;
+cout<<"~a is "<<~a<<endl;
+cout<<"a>> is "<<(a>>1)<<endl;
+cout<<"a<< is "<<(a<<1)<<endl;
+cout<<"a=b is "<<(a=b)<<endl;
+cout<<"~n & b is "<<~a <<endl;
+
+
+}
