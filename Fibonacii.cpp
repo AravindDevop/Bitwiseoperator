45651f771dd4b46f186995626ad16f1fe65db3d8
Aravindan <108742783+AravindDevop@users.noreply.github.com>
2022-07-05 20:04:10 +0530

    Add files via upload

---
+++ Fibonacii.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+    int n = 5;
+    int a = 0;
+    int b = 1;
+   // cout<<a<<" " <<b<<" " ;
+    for(int i=0;i<n;i++){
+
+
+       int  next = a+b;
+          cout<<next<<" ";;
+
+
+        a = b;
+        b = next;
+    }
+
+
+}
